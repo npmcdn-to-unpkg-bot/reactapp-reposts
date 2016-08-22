87717a9f5329a78f8249fe5f9bf0312df3ee8b35
semenov-k <kultur.smith@list.ru>
2016-08-22 22:07:44 +0500

    Test reactJS

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,53 @@
-/**
- * Created by semenov-k on 22.08.2016.
- */
-var TestClass = React.createClass({
+var my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+var News = React.createClass({
     render: function(){
-        return(<h2>test - {this.props.text}</h2>);
+        var data = this.props.data;
+        var news_template;
+        if(data.length > 0) {
+            news_template = data.map(function (data, index) {
+                return (
+                    <div key={index}>
+                        <p>{data.author}</p>
+                        <p>{data.text}</p>
+                    </div>
+                )
+            });
+        }
+        else{
+            news_template = <p>К сожалению новостей нет</p>
+        }
+        return(
+            <div className='news'>
+                {news_template}
+                <strong className={ data.length>0 ? '' : 'none' }>Всего новостей: {data.length}</strong>
+            </div>
+        )
+    }
+});
+var App = React.createClass({
+    render: function(){
+        return(
+            <div id='app'>
+                <h3>App News</h3>
+                <News data={my_news}/>
+            </div>
+        )
     }
 });
 ReactDOM.render(
-    <TestClass text="main" />,
+    <App />,
     document.getElementById('container')
 );
